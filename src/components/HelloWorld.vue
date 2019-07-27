8dfae62b83e95f13a3dd8def4dd645faeb8d760f
Jonathan Gravois <jongravois@gmail.com>
2019-07-27 06:12:11 -0500

    custom tailwind initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="p-12">
+    <table class="table w-full text-left">
+      <tr class="py-6">
+        <th>Classification</th>
+        <th>Dispersion</th>
+        <th>In Use</th>
+        <th>Storage</th>
+        <th>Retired</th>
+        <th>Other</th>
+        <th>&nbsp;</th>
+      </tr>
+      <tr class="py-6 shadow h-24">
+        <td colspan="6">
+          <input type="text" placeholder="filter list ..."
+                 class="w-1/2 shadow appearance-none border rounded w-90 py-2 px-3 text-grey-darker bg-white leading-tight">
+        </td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A220-100 (CS100)</td>
+        <td><1%</td>
+        <td>0</td>
+        <td>0</td>
+        <td>102</td>
+        <td>6</td>
+        <td><a href="#">View</a></td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A220-300 (CS300)</td>
+        <td><1%</td>
+        <td>0</td>
+        <td>0</td>
+        <td>151</td>
+        <td>29</td>
+        <td><a href="#">View</a></td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A300</td>
+        <td><1%</td>
+        <td>0</td>
+        <td>0</td>
+        <td>209</td>
+        <td>31</td>
+        <td><a href="#">View</a></td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A300-600</td>
+        <td><1%</td>
+        <td>24</td>
+        <td>41</td>
+        <td>105</td>
+        <td>39</td>
+        <td><a href="#">View</a></td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A310</td>
+        <td><1%</td>
+        <td>40</td>
+        <td>35</td>
+        <td>75</td>
+        <td>0</td>
+        <td><a href="#">View</a></td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A318</td>
+        <td><1%</td>
+        <td>30</td>
+        <td>10</td>
+        <td>64</td>
+        <td>12</td>
+        <td><a href="#">View</a></td>
+      </tr>
+      <tr class="py-6 shadow h-8">
+        <td>A319</td>
+        <td>2.3%</td>
+        <td>860</td>
+        <td>100</td>
+        <td>40</td>
+        <td>0</td>
+        <td><a href="#">View</a></td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'FleetManagerHome'
 }
 </script>
 
